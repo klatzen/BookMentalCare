861922999c4ab71cb916e408872db4079c5bb95b
Kristine El-Banna <1054336@ucn.dk>
2017-05-02 09:07:51 +0200

    Base

---
+++ BookMentalCare/BookMentalCareCore/DAL/IRessourceRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookMentalCareCore.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMentalCareCore.DAL
+{
+    public interface IRessourceRepository
+    {
+        Ressource LoadRessource(int id);
+        List<Ressource> LoadAllRessources();
+        Ressource SaveRessource(Ressource ressource);
+        bool RemoveRessource(int id);
+
+    }
+}
---
+++ BookMentalCare/BookMentalCareCore/DAL/RessourceRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookMentalCareCore.ModelLayer;
+
+namespace BookMentalCareCore.DAL
+{
+    public class RessourceRepository : BaseRepository, IRessourceRepository
+    {
+        public List<Ressource> LoadAllRessources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ressource LoadRessource(int id)
+        {
+           return dbContext.Ressources.FirstOrDefault(x => x.Id == id);
+            
+        }
+
+        public bool RemoveRessource(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ressource SaveRessource(Ressource ressource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
