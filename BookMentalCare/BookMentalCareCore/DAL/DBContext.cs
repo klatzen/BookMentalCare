50798aa17af35d07a4dc1cdf8aa05ec63d4c2b70
klatzen <klatzen2@gmail.com>
2017-05-02 09:07:14 +0200

    Good

---
+++ BookMentalCare/BookMentalCareCore/DAL/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMentalCareCore.DAL
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("MentalCareDB")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ BookMentalCare/BookMentalCareCore/DAL/IEmployeeRep.cs
@@ -0,0 +1,20 @@
+﻿using BookMentalCareCore.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMentalCareCore.DAL
+{
+    public interface IEmployeeRep
+    {
+        bool SaveEmployee(Employee e);
+
+        bool DeleteEmployee(Employee e);
+
+        Employee FindEmployee(string initials);
+
+        List<Employee> GetEmployees();
+    }
+}
