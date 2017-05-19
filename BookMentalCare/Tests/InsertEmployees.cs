5057bec64cc4256b074c9197ce49c5d17bb9492e
Kristine El-Banna <1054336@ucn.dk>
2017-05-19 11:21:18 +0200

    Employee create i test

--- BookMentalCare/Tests/InsertEmployees.cs
+++ BookMentalCare/Tests/InsertEmployees.cs
@@ -15,6 +15,9 @@ namespace Tests
             DepartmentFacade depFac = new DepartmentFacade();
 
             Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Andreas", LNAME = "Gelardi", INITIALS = "AG", PASSWORD = "1234", TITLE = "Master", DEPARTMENT = depFac.FindDepartment(1)}));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Kristine", LNAME = "El-Banna", INITIALS = "KE", PASSWORD = "1234", TITLE = "Leder", DEPARTMENT = depFac.FindDepartment(1)}));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Christian", LNAME = "Klattrup", INITIALS = "CK", PASSWORD = "1234", TITLE = "Læge", DEPARTMENT = depFac.FindDepartment(2) }));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Darlene", LNAME = "Frandsen", INITIALS = "DF", PASSWORD = "1234", TITLE = "Sygeplejerske", DEPARTMENT = depFac.FindDepartment(3)}));
         }
     }
 }
