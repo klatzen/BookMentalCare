f6f19450edb0c26757594398e8c0d5d1d75a24cc
klatzen <klatzen2@gmail.com>
2017-05-22 09:59:09 +0200

    pen-is

--- BookMentalCare/Tests/InsertEmployees.cs
+++ BookMentalCare/Tests/InsertEmployees.cs
@@ -14,10 +14,10 @@ namespace Tests
             EmployeeFacade empFac = new EmployeeFacade();
             DepartmentFacade depFac = new DepartmentFacade();
 
-            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Andreas", LNAME = "Gelardi", INITIALS = "AG", PASSWORD = "1234", TITLE = "Master", DEPARTMENT = depFac.FindDepartment(1)}));
-            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Kristine", LNAME = "El-Banna", INITIALS = "KE", PASSWORD = "1234", TITLE = "Leder", DEPARTMENT = depFac.FindDepartment(1)}));
-            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Christian", LNAME = "Klattrup", INITIALS = "CK", PASSWORD = "1234", TITLE = "Læge", DEPARTMENT = depFac.FindDepartment(2) }));
-            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Darlene", LNAME = "Frandsen", INITIALS = "DF", PASSWORD = "1234", TITLE = "Sygeplejerske", DEPARTMENT = depFac.FindDepartment(3)}));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Andreas", LNAME = "Gelardi",  PASSWORD = "1234", TITLE = "Master", DEPARTMENT = depFac.FindDepartment(1)}));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Kristine", LNAME = "El-Banna", PASSWORD = "1234", TITLE = "Leder", DEPARTMENT = depFac.FindDepartment(1)}));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Christian", LNAME = "Klattrup", PASSWORD = "1234", TITLE = "Læge", DEPARTMENT = depFac.FindDepartment(2) }));
+            Assert.IsTrue(empFac.SaveEmployee(new Employee() { FNAME = "Darlene", LNAME = "Frandsen", PASSWORD = "1234", TITLE = "Sygeplejerske", DEPARTMENT = depFac.FindDepartment(3)}));
         }
     }
 }
